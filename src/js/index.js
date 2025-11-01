0d1885a51666df2377b692d544c9d1227a46ec8e
Paulo Silva <paulofernando78@gmail.com>
2025-11-01 03:23:18 -0300

    wip

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,13 @@
+import {
+  darkMode,
+  lightMode,
+} from "../assets/images/svg-imports";
+
 const menuBtn = document.querySelector("wc-button[icon='menu']");
 const navBar = document.querySelector("nav");
 const navLinks = document.querySelectorAll("nav li a");
 
-menuBtn.addEventListener("nav-click", (e) => {
+menuBtn.addEventListener("nav-click", () => {
   navBar.classList.toggle("visible");
 });
 
@@ -12,10 +17,11 @@ navLinks.forEach((link) => {
   });
 });
 
-const darkMode = document.querySelector("wc-button[icon='darkMode']");
+const darkModeBtn = document.querySelector("wc-button[icon='darkMode']");
 
-darkMode.addEventListener("click", () => {
+darkModeBtn.addEventListener("click", () => {
   const current = document.body.getAttribute("data-theme");
   const newTheme = current === "dark" ? "light" : "dark";
   document.body.setAttribute("data-theme", newTheme);
+  darkModeBtn.innerHTML = newTheme === "dark" ? darkMode : lightMode;
 });
